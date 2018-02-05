f208791d07c67d08a351cbc5523440d692c2ebdd
jskyzero <jskyzero@outlook.com>
2018-02-05 18:36:56 +0800

    :sparkles: begin change view structure.

--- MoePicture/App.xaml.cs
+++ MoePicture/App.xaml.cs
@@ -69,7 +69,7 @@ namespace MoePicture
                     // 当导航堆栈尚未还原时，导航到第一页，
                     // 并通过将所需信息作为导航参数传入来配置
                     // 参数
-                    rootFrame.Navigate(typeof(Views.Shell), e.Arguments);
+                    rootFrame.Navigate(typeof(Views.MainPage), e.Arguments);
                 }
                 // 确保当前窗口处于活动状态
                 Window.Current.Activate();
---
+++ MoePicture/Views/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace MoePicture.Views
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            //var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+
+            //if (args.IsSettingsInvoked)
+            //{
+            //    ContentFrame.Navigate(typeof(SettingsPage));
+            //}
+            //else
+            //{
+            //    switch (args.InvokedItem)
+            //    {
+            //        case loader.GetString("HomeNavItem.Content"):
+            //            ContentFrame.Navigate(typeof(HomePage));
+            //            break;
+
+            //        case loader.GetString("AppsNavItem.Content"):
+            //            ContentFrame.Navigate(typeof(AppsPage));
+            //            break;
+
+            //        case loader.GetString("GamesNavItem.Content"):
+            //            ContentFrame.Navigate(typeof(GamesPage));
+            //            break;
+
+            //        case loader.GetString("MusicNavItem.Content"):
+            //            ContentFrame.Navigate(typeof(MusicPage));
+            //            break;
+
+            //        case loader.GetString("ContentNavItem.Content"):
+            //            ContentFrame.Navigate(typeof(MyContentPage));
+            //            break;
+            //    }
+            //}
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            //if (args.IsSettingsSelected)
+            //{
+            //    ContentFrame.Navigate(typeof(SettingsPage));
+            //}
+            //else
+            //{
+
+            //    NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+            //    switch (item.Tag)
+            //    {
+            //        case "home":
+            //            ContentFrame.Navigate(typeof(HomePage));
+            //            break;
+
+            //        case "apps":
+            //            ContentFrame.Navigate(typeof(AppsPage));
+            //            break;
+
+            //        case "games":
+            //            ContentFrame.Navigate(typeof(GamesPage));
+            //            break;
+
+            //        case "music":
+            //            ContentFrame.Navigate(typeof(MusicPage));
+            //            break;
+
+            //        case "content":
+            //            ContentFrame.Navigate(typeof(MyContentPage));
+            //            break;
+            //    }
+            //}
+        }
+    }
+}
