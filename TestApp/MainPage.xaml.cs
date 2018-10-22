ed7703228e2112470f983fe29dfbb5099b6d3262
jskyzero <jskyzero@outlook.com>
2018-10-22 11:10:01 +0800

    mv debug page to libs and use ugly IAsyncOperation make it work

--- TestApp/MainPage.xaml.cs
+++ TestApp/MainPage.xaml.cs
@@ -34,19 +34,8 @@ namespace TestApp
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            CoreApplicationView newView = CoreApplication.CreateNewView();
-            int newViewId = 0;
-            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                Frame frame = new Frame();
-                frame.Navigate(typeof(LogPage), null);
-                Window.Current.Content = frame;
-                // You have to activate the window in order to show it later.
-                Window.Current.Activate();
-
-                newViewId = ApplicationView.GetForCurrentView().Id;
-            });
-            bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
+            var logWindows = new JskyUwpLibs.LogWindows();
+            await logWindows.GetUriContentAsync("123");
         }
 
         void DispatcherTimerSetup()
---
+++ UwpLibs/LogWindows.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace JskyUwpLibs
+{
+    public sealed class LogWindows 
+    {
+
+        public IAsyncOperation<string> GetUriContentAsync(string uri)
+        {
+            return this.GetUriContentAsynHelper(uri).AsAsyncOperation();
+        }
+
+        private async Task<string> GetUriContentAsynHelper(string uri)
+        {
+            CoreApplicationView newView = CoreApplication.CreateNewView();
+            int newViewId = 0;
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Frame frame = new Frame();
+                frame.Navigate(typeof(LogPage), null);
+                Window.Current.Content = frame;
+                // You have to activate the window in order to show it later.
+                Window.Current.Activate();
+
+                newViewId = ApplicationView.GetForCurrentView().Id;
+            });
+            await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
+            return "123";
+        }
+
+    }
+}
