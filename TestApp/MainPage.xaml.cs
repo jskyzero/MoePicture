6315655a10d88642979983a511ee6062683df1f0
jskyzero <jskyzero@outlook.com>
2018-10-30 20:56:06 +0800

    finish custom control stack panel

--- TestApp/MainPage.xaml.cs
+++ TestApp/MainPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Navigation;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using Windows.UI.ViewManagement;
+using System.Drawing;
 
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
@@ -26,33 +27,52 @@ namespace TestApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        UIElementCollection Children = null;
+
         public MainPage()
         {
             this.InitializeComponent();
-            DispatcherTimerSetup();
-
             JskyUwpLibs.LogWindows.CreateLogWindowsAsync(2);
+            Children = TestControl.Children;
         }
 
-        void DispatcherTimerSetup()
+        private Windows.UI.Color GetRandomColor()
         {
-            DispatcherTimer dispatcherTimer;
-            dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Tick += dispatcherTimer_Tick;
-            dispatcherTimer.Start();
+            Random rnd = new Random();
+            Byte[] b = new Byte[3];
+            rnd.NextBytes(b);
+            Windows.UI.Color color = Windows.UI.Color.FromArgb(255, b[0], b[1], b[2]);
+            return color;
         }
 
-        void RandomMemoryUse()
+        private UIElement CreateNewUIElement()
         {
-            var size = (new Random((int)DateTimeOffset.Now.ToUnixTimeSeconds())).Next(1_000_000, 10_000_000);
-            var array = new List<int>(size) { 0 };
+            var rect = new Windows.UI.Xaml.Shapes.Rectangle();
+            rect.Width = 96;
+            rect.Height = 96;
+            rect.Margin = new Thickness(1);
+            rect.Fill = new SolidColorBrush(GetRandomColor());
+            return rect;
         }
 
-        void dispatcherTimer_Tick(object sender, object e)
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
         {
-            DateTimeOffset time = DateTimeOffset.Now;
-            RandomMemoryUse();
+            Children.Add(CreateNewUIElement());
         }
+
+        private void Insert_Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Remove_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Reset_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
